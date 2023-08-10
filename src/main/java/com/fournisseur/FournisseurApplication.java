779db95965ee3eac16f5db83a349ba82fddd9936
xavier_ng <xavierngaba@gmail.com>
2023-08-10 21:48:33 +0200

    fournisseur : init project

---
+++ src/main/java/com/fournisseur/FournisseurApplication.java
@@ -0,0 +1,13 @@
+package com.fournisseur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FournisseurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FournisseurApplication.class, args);
+	}
+
+}
